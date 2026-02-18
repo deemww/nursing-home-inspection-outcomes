c02b9c11070b864f880efe7302f4335c3a35b075
deemww <deem@uchicago.edu>
2026-02-18 06:30:20 -0600

    Update nursing home graphs.py

--- nursing home graphs.py
+++ nursing home graphs.py
@@ -9,7 +9,7 @@ st.markdown(
     <style>
 
     [data-testid="stSidebar"] {
-    background-color: #A6A6A6 !important;
+    background-color: #D9D9D9 !important;
 }
 
     /* ---- Gotham font faces ---- */
